af722a89d9b859afa0d8f970890ca2f591ad0f34
Farah <abdirashid.farah@adesso.de>
2026-06-10 09:52:05 +0200

    feat(k8s): add ingress routing for frontend and backend

--- infra/build.sh
+++ infra/build.sh
@@ -10,9 +10,11 @@ docker build \
   -t frontend-ai-deskhelp:latest \
   -f ../ai-deskhelp-frontend/frontend-Docker-file/Dockerfile ..
 
+# minikube addons enable ingress : nur einmalig notwendig, damit Ingress Controller installiert wird
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" #Dirname : dirname = Entferne den Dateinamen und gib nur den Ordner zurück
 
 kubectl apply -f "$SCRIPT_DIR/k8s/db-deployment.yaml" # was ausgeführt wird /Users/abdirashid.farah/work/ai-helpdesk/infra/k8s/db-deployment.yaml
 kubectl apply -f "$SCRIPT_DIR/k8s/frontend-deployment.yaml"
 kubectl apply -f "$SCRIPT_DIR/k8s/backend-deployment.yaml"
+kubectl apply -f "$SCRIPT_DIR/k8s/web-ingress.yaml"
